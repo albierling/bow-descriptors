0f85e9bfe3de2ed8b6a75e4a31a858a3c22948ef
albierling <84974515+albierling@users.noreply.github.com>
2024-07-15 10:24:46 +0200

    Update streamlit_app.py
    changed title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("BOW - A Standardised Lexicon of Body Odour Words")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
